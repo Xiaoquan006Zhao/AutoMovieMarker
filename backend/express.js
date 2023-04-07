0c3b940bee98cebfc7c2e1cece1446c7859378c0
Xiaoquan006Zhao <1009642031@qq.com>
2023-04-06 22:56:16 -0500

    host mysql on planetscale

--- backend/express.js
+++ backend/express.js
@@ -1,14 +1,33 @@
 const express = require("express");
-const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const EMOTION = require("./emotions");
 const CLIP = require("./clips");
 const MOVIE = require("./movies");
-
 const app = express();
 const port = 8080;
+
+// const mysql = require("mysql");
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "movies",
+//   charset: "utf8mb4",
+//   collation: "utf8mb4_general_ci",
+// });
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log("Connected to MySQL database");
+// });
+
+require("dotenv").config();
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
 
 app.use(
   function (req, res, next) {
@@ -18,20 +37,6 @@ app.use(
   cors(),
   bodyParser.json()
 );
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "movies",
-  charset: "utf8mb4",
-  collation: "utf8mb4_general_ci",
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL database");
-});
 
 const GET = "GET";
 const POST = "POST";
