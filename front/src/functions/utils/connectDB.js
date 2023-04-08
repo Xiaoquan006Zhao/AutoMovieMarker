050597e5635dd56dda9f5e7374cd228f5a7016b6
Xiaoquan006Zhao <1009642031@qq.com>
2023-04-08 02:10:58 -0500

    change connection to planetScale with netlify env variable

--- front/src/functions/utils/connectDB.js
+++ front/src/functions/utils/connectDB.js
@@ -1,18 +1,24 @@
 const mysql = require("mysql2/promise");
 
+const database_url = new URL(process.env.DATABASE_URL);
+const dbHost = database_url.hostname;
+const dbPort = database_url.port;
+const dbName = database_url.pathname.slice(1);
+const dbUser = database_url.username;
+const dbPassword = database_url.password;
+
 let pool;
 
 async function createPool() {
   if (pool) return pool;
 
   pool = await mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "movies",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+    host: dbHost,
+    port: dbPort,
+    database: dbName,
+    user: dbUser,
+    password: dbPassword,
+    ssl: { rejectUnauthorized: true },
   });
 
   return pool;
