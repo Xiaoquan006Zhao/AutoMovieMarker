b604014358e891e2e0c8511c7bc773e00db09eb8
Xiaoquan006Zhao <1009642031@qq.com>
2023-08-19 23:43:40 -0500

    Setting new default image url (Forgot the backend is now netflify's servless function)

--- front/src/functions/_insertClip.js
+++ front/src/functions/_insertClip.js
@@ -5,10 +5,9 @@ const template = require("./utils/templateFunction");
 exports.handler = async (event, context) => {
   const { movie_id } = event.queryStringParameters;
 
-  const queryInsertClip = `INSERT INTO clips (movie_id, image_url) VALUES (?, ?)`;
+  const queryInsertClip = `INSERT INTO clips (movie_id) VALUES (?)`;
   const response = await template.templatedQuery(event, queryInsertClip, [
     movie_id,
-    "no image",
   ]);
 
   return template.templateSend(response, ["insertId"]);
